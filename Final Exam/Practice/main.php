8bf675484f9f8a985d418f512a078f2a6e994cfd
_gu_npe.tnnx__ <thananop.klp@gmail.com>
2024-11-24 17:23:34 +0700

    Create main.php

---
+++ Final Exam/Practice/main.php
@@ -0,0 +1,17 @@
+<?php include 'connection.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>เว็บไซต์ท่องเที่ยว</title>
+</head>
+<body>
+  <h1>ยินดีต้อนรับสู่เว็บไซต์ท่องเที่ยว</h1>
+  <ul>
+    <li><a href="create_city.php">สร้างเมืองเป้าหมาย</a></li>
+    <li><a href="add_traveler.php">เพิ่มรายชื่อผู้สนใจเที่ยว</a></li>
+    <li><a href="select_city_traveler.php">ขายทัวร์</a></li>
+    <li><a href="add_tour.php">เพิ่มทัวร์</a></li>
+    <li><a href="view_tour.php">ดูทัวร์</a></li>
+  </ul>
+</body>
+</html>
